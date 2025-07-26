b016ec9d0f6a3839f689e55344d8baba1d125c73
ashirveuz <ashir@veuz.in>
2025-07-26 11:18:47 +0530

    route changed

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,11 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\WishListController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ContactController;
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -15,6 +16,30 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::redirect('/', '/home');
+
+Route::prefix('home')->controller(HomeController::class)->group(function () {
+    Route::get('/', 'index')->name('website.home');
+});
+
+Route::prefix('wish-list')->controller(WishListController::class)->group(function () {
+    Route::get('/', 'index')->name('website.wish-list');
+});
+
+Route::prefix('/')->controller(ProductController::class)->group(function () {
+    Route::get('/products', 'index')->name('website.products');
+    Route::get('/product', 'show')->name('website.product');
+});
+
+
+Route::prefix('about-us')->controller(AboutController::class)->group(function () {
+    Route::get('/', 'index')->name('website.about');
+});
+
+Route::prefix('contact-us')->controller(ContactController::class)->group(function () {
+    Route::get('/', 'index')->name('website.contact');
 });
 
 require __DIR__.'/auth.php';
